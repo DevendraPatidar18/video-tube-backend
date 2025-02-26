1ebcaccc160aaf7f3f8b8f690189e530baa0a1f3
DevendraPatidar18 <patidardevendra994@Gmail.com>
2025-02-26 14:33:21 +0530

    testin api

--- index.js
+++ index.js
@@ -5,23 +5,7 @@ const express = require('express')
 const app = express()
 
 const userData = {
-    "login": "hiteshchaudhary",
-    "id": 17576078,
-    "node_id": "MDQ6VXNlcjE3NTc2MDc4",
-    "avatar_url": "https://avatars.githubusercontent.com/u/17576078?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/hiteshchaudhary",
-    "html_url": "https://github.com/hiteshchaudhary",
-    "followers_url": "https://api.github.com/users/hiteshchaudhary/followers",
-    "following_url": "https://api.github.com/users/hiteshchaudhary/following{/other_user}",
-    "gists_url": "https://api.github.com/users/hiteshchaudhary/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/hiteshchaudhary/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/hiteshchaudhary/subscriptions",
-    "organizations_url": "https://api.github.com/users/hiteshchaudhary/orgs",
-    "repos_url": "https://api.github.com/users/hiteshchaudhary/repos",
-    "events_url": "https://api.github.com/users/hiteshchaudhary/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/hiteshchaudhary/received_events",
-    "type": "User",
+
     "user_view_type": "public",
     "site_admin": false,
     "name": null,
