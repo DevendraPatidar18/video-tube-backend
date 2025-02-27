03b4836a6f96c56a52617b0b73e217b3ba14eaed
DevendraPatidar18 <patidardevendra994@Gmail.com>
2025-02-27 22:06:48 +0530

    Configure Database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-require('dotenv').config()
-
-const express = require('express')
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
+import express from"express";
+import {DB_NAME} from "./constants.js"
+import connectDB from "./database/connetctDB.js"
 
 const app = express()
+await connectDB()
+
 
 const userData = {
 
