d3c22ce8164195138543e8ae060ffe8c34cb3aef
DevendraPatidar18 <patidardevendra994@Gmail.com>
2025-02-28 11:09:01 +0530

    try to fix bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
+//import dotenv from 'dotenv';
+//dotenv.config({ path: '.env' });
+import 'dotenv/config';
 import express from"express";
 import {DB_NAME} from "./constants.js"
 import connectDB from "./database/connetctDB.js"
