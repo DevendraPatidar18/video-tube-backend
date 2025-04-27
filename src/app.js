eaa26153e457c589c343f933ea20c44257e032ed
DevendraPatidar18 <patidardevendra994@Gmail.com>
2025-04-27 20:04:07 +0530

    Set server data limit

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ app.use(cookieParser())
  import {userRouter} from "./routes/user.routes.js"
 import { videoRouter } from "./routes/video.routes.js";
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
  //router declaration
  app.use("/api/v1/users",userRouter)
